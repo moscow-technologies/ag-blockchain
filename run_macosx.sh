7677a30a90447dace3a2cca9169fdafdbc7b2e32
smashin <smashin@33slona.ru>
2017-11-29 16:33:43 +0300

    One more bootnode

--- run_macosx.sh
+++ run_macosx.sh
@@ -23,4 +23,4 @@ then
 fi
 
 wget  -q https://raw.githubusercontent.com/moscow-technologies/ag-blockchain/master/parity/config/chain.json -O chain.json
-parity ui --chain chain.json --bootnodes enode://9076c143a487aa163437a86f7d009f257f405c50bb2316800b9c9cc40e5a38fef5b414a47636ec38fdabc8a1872b563effa8574a7f8f85dc6bde465c368f1bf5@213.79.88.177:30303
+parity ui --chain chain.json --bootnodes enode://1412ee9b9e23700e4a67a8fe3d8d02e10376b6e1cb748eaaf8aa60d4652b27872a8e1ad65bb31046438a5d3c1b71b00ec3ce0b4b42ac71464b28026a3d0b53af@213.79.88.178:30303,enode://9076c143a487aa163437a86f7d009f257f405c50bb2316800b9c9cc40e5a38fef5b414a47636ec38fdabc8a1872b563effa8574a7f8f85dc6bde465c368f1bf5@213.79.88.177:30303
